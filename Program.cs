045a9c59287cb4599ba5617fe80ad580610fad0c
LimaLeo13 <limaleo13@hotmail.com>
2023-08-30 15:18:23 -0300

    ContaBancaria

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 1;
+
+
+            do
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("                 |%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|");
+                Console.WriteLine("                 |                                                                  |");
+                Console.WriteLine("                 |                           Banco do Br                            |");
+                Console.WriteLine("                 |                                                                  |");
+                Console.WriteLine("                 |&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&|");
+                Console.WriteLine("                 |                                                                  |");
+                Console.WriteLine("                 |                                                                  |");
+                Console.WriteLine("                 |                 1 - Criar Conta                                  |");
+                Console.WriteLine("                 |                 2 - Listar Todas as contas                       |");
+                Console.WriteLine("                 |                 3 - Buscar conta por número                      |");
+                Console.WriteLine("                 |                 4 - Atualizar dados da conta                     |");
+                Console.WriteLine("                 |                 5 - Apagar conta                                 |");
+                Console.WriteLine("                 |                 6 - Sacar                                        |");
+                Console.WriteLine("                 |                 7 - Depositar                                    |");
+                Console.WriteLine("                 |                 8 - Transferir Valores Entre Contas              |");
+                Console.WriteLine("                 |                 9 - Sair                                         |");
+                Console.WriteLine("                 |                                                                  |");
+                Console.WriteLine("                 |                                                                  |");
+                Console.WriteLine("                 |%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|");
+                Console.WriteLine("                 |                                                                  |");
+                Console.WriteLine("                 |                Entre com a opção desejada                        |");
+                Console.WriteLine("                 |                                                                  |");
+                Console.WriteLine("                 |%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|");
+                opcao = Convert.ToInt32(Console.ReadLine());
+              
+            
+                switch (opcao)
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    case 4:
+                        break;
+
+                    case 5:
+                        break;
+
+                    case 6:
+                        break;
+
+                    case 7:
+                        break;
+
+                    case 8:
+                        break;
+
+                    case 9: 
+                        {
+                            Console.WriteLine("Obrigado por utilizar o nosso banco!!");
+                            System.Environment.Exit(0);
+                        }
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Essa opção não existe!");
+
+                        break;
+                }
+
+
+            } while (true);
+
+
+        }
+    }
+}
